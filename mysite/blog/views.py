5230d0714fbb9f02d54687d365a23b0f4ea03a45
zulcia <kinga.czenczek@gmail.com>
2021-01-01 19:06:27 +0100

    szablony + static + paginacja

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,16 +1,36 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+
+# def post_list(request):
+#     posts = Post.published.all()
+#     return render(
+#         request,
+#         'blog/post/list.html',
+#         {
+#             'posts': posts,
+#         }
+#     )
 
 
 def post_list(request):
-    posts = Post.published.all()
-    return render(
-        request,
-        'blog/post/list.html',
-        {
-            'posts': posts,
-        }
-    )
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)   # liczba postów na str
+    page = request.GET.get('page')          # nr bieżącej str
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # jeśli page nie jest integerem, pobież 1 str
+        posts = paginator.page(1)
+    except EmptyPage:
+        # jesli page > od ost str, wyświetl ost str
+        # ost str (paginator.num_pages)
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  'blog/post/list.html',
+                  {'page': page,
+                   'posts': posts})
 
 
 def post_details(request, year, month, day, post):
